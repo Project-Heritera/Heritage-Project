16a0fce7f329c9a69e3e5a42eda098ada7d7cf6a
Malik <m.malik62205@gmail.com>
2025-09-18 14:25:53 -0500

    made frontend react with vite in typescript. May change depending on hudsons opinion. For not though made a simple login page with a layout for states(view readme with why we should use zustland instead of react states). as well as a utils folder for all ts files that will be indepenent from backend and simply consists of helper functions for react frontend. Also updated readme to contain a layout that gpt recommended for es

---
+++ frontend/heritage-project-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import AuthLogin from "./components/auth";
+function App() {
+  return (
+    <>
+      <div>
+        <AuthLogin />
+      </div>
+    </>
+  );
+}
+
+export default App;
