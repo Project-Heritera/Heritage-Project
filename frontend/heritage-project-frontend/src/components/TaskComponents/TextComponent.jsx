d91de9529d22bd0d4e41628f64d63e1c719761c2
clayton <claytonhouser10@gmail.com>
2025-10-08 13:47:39 -0500

    Adding storybook set up for testing, viewing, and documenting

---
+++ frontend/heritage-project-frontend/src/components/TaskComponents/TextComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TextComponent = () => {
+    
+    return (
+        <div>
+            <h1>Hello Preview.js!</h1>
+        </div>
+    );
+};
+
+export default TextComponent;
