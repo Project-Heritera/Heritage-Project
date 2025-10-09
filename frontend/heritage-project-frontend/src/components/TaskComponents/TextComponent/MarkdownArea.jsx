8b2ad8075f177b7334d15d4ad5b1eb70a5a16086
clayton <claytonhouser10@gmail.com>
2025-10-09 10:51:52 -0500

    Adding ability to paste in markdown text and have it render in the editor

--- frontend/heritage-project-frontend/src/components/TaskComponents/TextComponent/MarkdownArea.jsx
+++ frontend/heritage-project-frontend/src/components/TaskComponents/TextComponent/MarkdownArea.jsx
@@ -1,13 +1,14 @@
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import { marked } from 'marked';
+import { PasteMarkdown } from '../../../services/TaskComponents/TextComponent/MarkdownPaste';
 
 const MarkdownArea = ({initText}) => {
     //Convert mark down text into html to render right at start
     const initHtml = marked(initText)
 
     const editor = useEditor({
-        extensions: [StarterKit],//Include extensios
+        extensions: [StarterKit, PasteMarkdown],//Include extensios
         content: initHtml//Initial text
     })
 
