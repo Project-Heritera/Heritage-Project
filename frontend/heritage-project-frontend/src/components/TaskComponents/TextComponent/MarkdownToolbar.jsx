180ddce6f01b146f55a3809454ae633cc85e7b5b
clayton <claytonhouser10@gmail.com>
2025-10-13 13:42:37 -0500

    Adding toolbar styling

--- frontend/heritage-project-frontend/src/components/TaskComponents/TextComponent/MarkdownToolbar.jsx
+++ frontend/heritage-project-frontend/src/components/TaskComponents/TextComponent/MarkdownToolbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Markdown from "../../Utilities/Markdown"
+import "../../../styles/Components/TaskComponents/TextComponent.css"
 
 const Markdowntoolbar = ({children}) => {
     return (
-        <div>
+        <div className="toolbar">
             {children}
         </div>
     );
