00b62ae87e9b2031956a58e7aed1d74fd43528ce
William Morales <wsmbuis5224@gmail.com>
2025-11-05 15:19:09 -0600

    adding urls for new apis

--- backend/apps/website/urls.py
+++ backend/apps/website/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # course apis
+    path("courses/create_course", views.create_course),
+    path("courses/", views.get_courses),
+    # section apis
+    path("courses/<int:course_id>/create_section", views.create_room),
+    path("courses/<int:course_id>/sections/", views.get_rooms),
+    # room apis
     path("courses/<int:course_id>/sections/<int:section_id>/create_room/", views.create_room),
+    path("courses/<int:course_id>/sections/<int:section_id>/rooms/", views.get_rooms),
     path("courses/<int:course_id>/sections/<int:section_id>/rooms/<int:room_id>/", views.get_room),
     path("courses/<int:course_id>/sections/<int:section_id>/rooms/<int:room_id>/save/", views.save_room),
     path("courses/<int:course_id>/sections/<int:section_id>/rooms/<int:room_id>/publish/", views.publish_room),
