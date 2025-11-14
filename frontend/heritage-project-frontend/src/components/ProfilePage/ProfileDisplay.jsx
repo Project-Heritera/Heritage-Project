359d40aa5e05d561dd5dc92bcd6ce3c3c6890e00
clayton <claytonhouser10@gmail.com>
2025-11-14 17:47:31 -0600

    Finished profile card

--- frontend/heritage-project-frontend/src/components/ProfilePage/ProfileDisplay.jsx
+++ frontend/heritage-project-frontend/src/components/ProfilePage/ProfileDisplay.jsx
@@ -6,9 +6,9 @@ import ProfileSummary from "./ProfileSummary/ProfileSummary";
 function ProfileDisplay() {
   return (
     <ProfileDiv>
-      <div className="flex gap-x-8">
+      <div className="flex gap-x-8 items-center">
         <ProfileImage profileImage={"http://localhost:5173/testimage.jpg"}/>
-        <div className="flex flex-col justify-between flex-1">
+        <div className="flex flex-col gap-y-12">
           <ProfileDescription name={"Harry Potter"} description={"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"}></ProfileDescription>
           <ProfileSummary></ProfileSummary>
         </div>
