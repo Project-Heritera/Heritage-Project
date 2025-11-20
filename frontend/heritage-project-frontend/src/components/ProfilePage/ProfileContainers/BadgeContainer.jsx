5ecc7fc43a93b0f57c58a2b58726481689c90a99
clayton <claytonhouser10@gmail.com>
2025-11-20 09:44:49 -0600

    Adding api request

--- frontend/heritage-project-frontend/src/components/ProfilePage/ProfileContainers/BadgeContainer.jsx
+++ frontend/heritage-project-frontend/src/components/ProfilePage/ProfileContainers/BadgeContainer.jsx
@@ -1,9 +1,14 @@
 import ProfileContainer from "./ProfileContainer";
 import Badge from "@/components/Common/Badge/Badge";
+import api from "../../../services/api"
 
 function BadgeContainer() {
+  const getBadges = () => {
+    const badges = api.get("/website/badges/")
+  }
   return (
     <ProfileContainer title={"Badges"} itemsPerRow={2}>
+        {console.log(getBadges())}
         <Badge image={"http://localhost:5173/testimage.jpg"}/>
         <Badge image={"http://localhost:5173/testimage.jpg"}/>
         <Badge image={"http://localhost:5173/testimage.jpg"}/>
