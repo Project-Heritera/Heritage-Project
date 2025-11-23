d2762012d9401487e3a6fe93f89e88ce284807fa
clayton <claytonhouser10@gmail.com>
2025-11-22 18:25:32 -0600

    Adding updated link for profile that retrieves the user when going to link /u/:username

--- frontend/heritage-project-frontend/src/pages/profile.jsx
+++ frontend/heritage-project-frontend/src/pages/profile.jsx
@@ -1,8 +1,11 @@
 import ProfileDisplay from "../components/ProfilePage/ProfileDisplay";
 import BadgeContainer from "@/components/ProfilePage/ProfileContainers/BadgeContainer";
+import { useParams } from "react-router-dom";
 
 
 function Profile() {
+  const {username} = useParams();
+  console.log("User is:", username)
   return (
     <div className="flex flex-col gap-6 p-6">
       <ProfileDisplay profImage={"http://localhost:5173/testimage.jpg"} name={"Harry Potter"} description={"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"}/>
