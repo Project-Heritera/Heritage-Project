f9514a92a7ff318b54daf122bce1c16f71e30f6d
clayton <claytonhouser10@gmail.com>
2025-11-22 18:01:29 -0600

    Set up badge api for currently logged in user

--- frontend/heritage-project-frontend/src/components/Common/Badge/Badge.jsx
+++ frontend/heritage-project-frontend/src/components/Common/Badge/Badge.jsx
@@ -2,12 +2,13 @@ import {Card, CardContent, CardTitle, CardDescription} from "../../ui/card"
 import BadgeImage from "./BadgeImage";
 
 function Badge({image, title}) {
+  console.log("Image is:", image)
   return (
     <Card>
         <CardContent className="badgeContent">
             {/* Display the Image */}
             <BadgeImage badgeImage={image}></BadgeImage>
-            <CardTitle>Creole Completed</CardTitle>
+            <CardTitle>{title}</CardTitle>
             <CardDescription>This badge is awared for the completion of the Creole Course</CardDescription>
         </CardContent>
     </Card>
--- frontend/heritage-project-frontend/src/components/ProfilePage/ProfileContainers/BadgeContainer.jsx
+++ frontend/heritage-project-frontend/src/components/ProfilePage/ProfileContainers/BadgeContainer.jsx
@@ -1,17 +1,38 @@
 import ProfileContainer from "./ProfileContainer";
 import Badge from "@/components/Common/Badge/Badge";
-import api from "../../../services/api"
+import api from "../../../services/api";
+import { useState, useEffect } from "react";
 
 function BadgeContainer() {
   const getBadges = () => {
     const badges = api.get("/website/badges/")
   }
+
+  const [badges, setBadges] = useState([]);
+
+  useEffect(() => {
+    const fetchBadges = async () => {
+      try {
+        const response = await api.get("/website/badges/");
+        console.log("API badges response:", response.data)
+        setBadges(response.data);
+      } catch (error) {
+        console.error("Error geting badges:", error)
+      }
+    };
+
+    fetchBadges();
+  }, []);
+
   return (
     <ProfileContainer title={"Badges"} itemsPerRow={3}>
-        {console.log(getBadges())}
-        <Badge image={"http://localhost:5173/testimage.jpg"}/>
-        <Badge image={"http://localhost:5173/testimage.jpg"}/>
-        <Badge image={"http://localhost:5173/testimage.jpg"}/>
+        {badges.map((badge) => (
+          <Badge
+            key={badge.title}
+            title={badge.title}
+            image={badge.image}
+          />
+        ))}
     </ProfileContainer>
   )
 }
