6ae238d75e8972b68163a3b9a36211d0481bce5c
Malik <m.malik62205@gmail.com>
2025-11-27 14:12:10 -0600

    made hcoice button ui chnages

--- frontend/heritage-project-frontend/src/components/TaskComponents/MultipleChoiceComponent/ChoiceButton.jsx
+++ frontend/heritage-project-frontend/src/components/TaskComponents/MultipleChoiceComponent/ChoiceButton.jsx
@@ -4,21 +4,29 @@ import { Button } from "@/components/ui/button";
 //Component used to render the choice button for the reading side of the component
 const ChoiceButton = ({ choiceId, text, isCorrect, selected, onClick }) => {
   return (
-    <Button
-     onClick={onClick}
-      className={`choice-button ${selected ? "selected" : ""}`}
-      style={{
-        backgroundColor: selected ? "" : "#f3f4f6",
-        color: selected ? "white" : "black",
-        border: selected ? "2px solid #2563eb" : "2px solid transparent",
-        padding: "0.5rem 1rem",
-        borderRadius: "0.5rem",
-        cursor: "pointer",
-        margin: "0.25rem 0",
-      }}
-    >
-      {choiceId}: {text}
-    </Button>
+  <Button
+  onClick={onClick}
+  className={`choice-button ${selected ? "selected" : ""}`}
+  style={{
+    backgroundColor: selected ? "" : "#f3f4f6",
+    color: selected ? "white" : "black",
+    padding: "0.5rem 1rem",
+    borderRadius: "0.5rem",
+    cursor: "pointer",
+    margin: "0.25rem 0",
+    minWidth: "40%"
+  }}
+>
+  <div
+    style={{
+      textAlign: "left",
+      whiteSpace: "pre-wrap",   // <-- makes spaces show
+      width: "100%"
+    }}
+  >
+    {`${choiceId}:     ${text}`}
+  </div>
+</Button>
   );
 };
 
