f7adce00a0877ee58a17678c018f1881b13a44ee
clayton <claytonhouser10@gmail.com>
2025-11-29 20:10:57 -0600

    Adding profile picture for logged in user. Backend needs fixing

--- frontend/heritage-project-frontend/src/assets/navbar/NavbarDropDown.jsx
+++ frontend/heritage-project-frontend/src/assets/navbar/NavbarDropDown.jsx
@@ -13,10 +13,27 @@ import { Separator } from "@/components/ui/separator"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { USER_NAME } from "@/services/LocalStorage";
 import SearchBar from "@/components/Common/Search/SearchBar";
+import { useEffect, useState } from "react";
+import api from "@/services/api";
 
 function NavbarDropDown() {
     const user = localStorage.getItem(USER_NAME)
     const navigate = useNavigate();
+    const [userObject, setUserObject] = useState(null)
+
+    useEffect(() => {
+        if (!user) {
+            return
+        }
+        try {
+            const response = api.get("/accounts/user_info/")
+            const userData = response.data
+            console.log("user object is:", userData)
+            setUserObject(userData)
+        } catch (error) {
+            console.error("Error geting user object: ", error)
+        }
+    })
 
     return (
         <Sheet>
@@ -66,31 +83,33 @@ function NavbarDropDown() {
                 </div>
 
                 {/* User Section */}
-                <div className="mt-auto px-6 mb-6">
-                    {/* Account Info */}
-                    <div className="flex items-center gap-3 mb-4 px-2">
-                        <Avatar>
-                            <AvatarImage src="https://github.com/shadcn.png" />
-                            <AvatarFallback>{user && user.charAt(0).toUpperCase()}</AvatarFallback>
-                        </Avatar>
-                        <div>
-                            <p className="font-medium">{user}</p>
-                            <p className="text-xs text-muted-foreground">clayton@example.com</p>
+                {userObject && (
+                    <div className="mt-auto px-6 mb-6">
+                        {/* Account Info */}
+                        <div className="flex items-center gap-3 mb-4 px-2">
+                            <Avatar>
+                                <AvatarImage src={userObject.profile_pic} />
+                                <AvatarFallback>{user && user.charAt(0).toUpperCase()}</AvatarFallback>
+                            </Avatar>
+                            <div>
+                                <p className="font-medium">{user}</p>
+                                <p className="text-xs text-muted-foreground">account</p>
+                            </div>
+                        </div>
+                        {/* Account Actions */}
+                        <div className="flex flex-col gap-2 ">
+                            <Link to="/settings" className="flex items-center gap-2 px-2 py-2 rounded-md hover:bg-accent text-sm">
+                                <Settings className="h-4 w-4" /> Settings
+                            </Link>
+                            <Link to={`/u/${user}`} className="flex items-center gap-2 px-2 py-2 rounded-md hover:bg-accent text-sm">
+                                <User className="h-4 w-4" /> Profile
+                            </Link>
+                            <button className="flex items-center gap-2 px-2 py-2 rounded-md hover:bg-red-100 text-red-600 text-sm w-full text-left">
+                                <LogOut className="h-4 w-4" /> Logout
+                            </button>
                         </div>
                     </div>
-                    {/* Account Actions */}
-                    <div className="flex flex-col gap-2 ">
-                        <Link to="/settings" className="flex items-center gap-2 px-2 py-2 rounded-md hover:bg-accent text-sm">
-                            <Settings className="h-4 w-4" /> Settings
-                        </Link>
-                        <Link to={`/u/${user}`} className="flex items-center gap-2 px-2 py-2 rounded-md hover:bg-accent text-sm">
-                            <User className="h-4 w-4" /> Profile
-                        </Link>
-                        <button className="flex items-center gap-2 px-2 py-2 rounded-md hover:bg-red-100 text-red-600 text-sm w-full text-left">
-                            <LogOut className="h-4 w-4" /> Logout
-                        </button>
-                    </div>
-                </div>
+                )}
             </SheetContent>
         </Sheet>
     )
