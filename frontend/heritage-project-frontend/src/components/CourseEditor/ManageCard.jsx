78b93500669f74a8fbb533929f9c76f2c3a59c45
clayton <claytonhouser10@gmail.com>
2025-12-04 13:28:33 -0600

    adding publish course button

--- frontend/heritage-project-frontend/src/components/CourseEditor/ManageCard.jsx
+++ frontend/heritage-project-frontend/src/components/CourseEditor/ManageCard.jsx
@@ -4,7 +4,8 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Link } from "react-router-dom"
 import { Button } from "../ui/button";
 
-export default function ManageCard({ courseId }) {
+export default function ManageCard({ courseId, accessType }) {
+    const isPublished = (accessType === "Published")
     return (
         <Card>
             <CardHeader>
@@ -14,6 +15,12 @@ export default function ManageCard({ courseId }) {
                         Course Settings
                     </Button>
                 </Link>
+                <CardTitle className="text-lg font-semibold">Manage access: {accessType}</CardTitle>
+                <div>
+                    <Button className="hover:shadow-lg transition-shadow cursor-pointer">
+                        {isPublished ? "Make Private" : "Make Public"}
+                    </Button>
+                </div>
             </CardHeader>
         </Card>
 
