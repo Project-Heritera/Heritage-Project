c46572dc15e7a7d0e524d4fe340b289c1f85e167
clayton <claytonhouser10@gmail.com>
2025-12-05 01:15:17 -0600

    Adding 2fa fixes

--- frontend/heritage-project-frontend/src/services/auth.js
+++ frontend/heritage-project-frontend/src/services/auth.js
@@ -1,15 +1,21 @@
 import api from './api'
-import { ACCESS_TOKEN, REFRESH_TOKEN, USER_NAME } from '../services/LocalStorage'; 
+import { ACCESS_TOKEN, REFRESH_TOKEN, USER_NAME } from '../services/LocalStorage';
 
 //This function/service handles login requests for JWT tokens during a login attempt. Requires input of username and password of user.
 //Tokens are stored in local storage.
-export async function login(username, password) {
+export async function login(username, password, authResponse) {
 	try {
 		//Attempt to request tokens from backend
-		const response = await api.post(`/accounts/login_step1/`, {
-			 username,
-			 password
-		});
+		let response = null
+		if (authResponse) {
+			response = authResponse
+		} else {
+			console.log("Using default login")
+			response = await api.post(`/accounts/login_step1/`, {
+				username,
+				password
+			});
+		}
 
 		if (response.data.mfa_required) {
 			return response.data
@@ -25,11 +31,11 @@ export async function login(username, password) {
 		localStorage.setItem(USER_NAME, username);
 		console.log('Login successful', response.data);
 		return response.data
-	} 
+	}
 	catch (error) {
 		if (error.response) {
-      		throw { status: error.response.status, data: error.response.data };
-    	}
+			throw { status: error.response.status, data: error.response.data };
+		}
 		else if (error.request) {
 			throw { status: null, data: null, message: 'No response received from server' };
 		}
